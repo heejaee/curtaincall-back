376d103c03fa8a63776c87fe8cf0012ba6cbd010
박희재 <=>
2025-05-02 11:36:25 +0900

    refactor -RestController로 변경

--- src/main/java/com/backstage/curtaincall/category/controller/CategoryController.java
+++ src/main/java/com/backstage/curtaincall/category/controller/CategoryController.java
@@ -7,8 +7,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,49 +25,38 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
-    // 삭제 되지않는 것만 전체 조회
+    // 삭제되지 않은 것만 전체 조회
     @GetMapping
-    public ResponseEntity<List<CategoryDto>> findAllNotDeleted(){
-        List<CategoryDto> CategoryDtos = categoryService.findAllNotDeleted();
-
-        return new ResponseEntity<>(CategoryDtos, HttpStatus.OK);
+    public List<CategoryDto> findAllNotDeleted() {
+        return categoryService.findAllNotDeleted();
     }
 
-    // 삭제 된 것만 전체 조회
+    // 삭제된 것만 전체 조회
     @GetMapping("findAllDeleted")
-    public ResponseEntity<List<CategoryDto>> findAllDeleted(){
-        List<CategoryDto> CategoryDtos = categoryService.findAllDeleted();
-
-        return new ResponseEntity<>(CategoryDtos, HttpStatus.OK);
+    public List<CategoryDto> findAllDeleted() {
+        return categoryService.findAllDeleted();
     }
 
-
-    //생성
+    // 생성
     @PostMapping
-    public ResponseEntity<CategoryDto> create(@Valid @RequestBody CategoryDto categoryDto){
-
-        CategoryDto categoryDtoResponse = categoryService.save(categoryDto.getName(), categoryDto.getParentId());
-        return new ResponseEntity<>(categoryDtoResponse, HttpStatus.OK);
+    public CategoryDto create(@Valid @RequestBody CategoryDto categoryDto) {
+        return categoryService.save(categoryDto.getName(), categoryDto.getParentId());
     }
 
-
-    //카테고리 수정
+    // 수정
     @PutMapping
-    public ResponseEntity<CategoryDto> update(@Valid @RequestBody CategoryDto categoryDto){
-
-        CategoryDto categoryDtoResponse = categoryService.update(categoryDto.getName(), categoryDto.getId());
-        return new ResponseEntity<>(categoryDtoResponse, HttpStatus.OK);
+    public CategoryDto update(@Valid @RequestBody CategoryDto categoryDto) {
+        return categoryService.update(categoryDto.getName(), categoryDto.getId());
     }
 
-    //카테고리 삭제
+    // 삭제
     @DeleteMapping("{categoryId}")
-    @ResponseStatus(HttpStatus.OK) //본문이 필요하지 않으니 ResponseStatus 사용
-    public void delete(@PathVariable("categoryId") Long categoryId){
-
+    @ResponseStatus(HttpStatus.OK) // 본문 없음, 상태만 반환
+    public void delete(@PathVariable("categoryId") Long categoryId) {
         categoryService.delete(categoryId);
     }
 
-    //카테고리 복구
+    // 복구
     @PutMapping("restore/{categoryId}")
     @ResponseStatus(HttpStatus.OK)
     public void restore(@PathVariable("categoryId") Long categoryId) {
