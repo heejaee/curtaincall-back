ba75f2cab887a24fa5fa7368492b9c05a9d16673
박희재 <hjhjhj377021@gmail.com>
2025-07-11 15:12:12 +0900

    refactor - 안쓰는 스케줄러 정리

--- src/main/java/com/backstage/curtaincall/CurtaincallApplication.java
+++ src/main/java/com/backstage/curtaincall/CurtaincallApplication.java
@@ -11,7 +11,6 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
-//@EnableScheduling
 public class CurtaincallApplication {
 
     public static void main(String[] args) {
